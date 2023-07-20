213af859aeb141202e1d3d82d2772bbd562a6479
realasyed <armaansyed120@gmail.com>
2023-07-20 14:28:46 -0500

    input test

--- kd.py
+++ kd.py
@@ -3,9 +3,19 @@ import random
 import streamlit as st 
 import pandas as pd #pandas is how we get data into a data frame 
 
+#Taking data and putting it into a data frame with pandas
 dataPath = ('./filmStats/movie_statistic_dataset.csv')
 df = pd.read_csv(dataPath)
 
-random_index = random.randint(0, len(df) - 1) #takes a random int between 0 and the amount of data - 1.
-random_movie_title = df.loc[random_index, 'Title'] #From W3: "The loc property gets, or sets, the value(s) of the specified labels."
-print("Random Movie Title:", random_movie_title)
+#random_index = random.randint(0, len(df) - 1) #takes a random int between 0 and the amount of data - 1.
+#random_movie_title = df.loc[random_index, 'Title'] #From W3: "The loc property gets, or sets, the value(s) of the specified labels."
+#print("Random Movie Title:", random_movie_title)
+#The commented code above didnt work because I didn't specify what particular column I need to len.
+#this was not especially relevant anyways. i wasted some time.
+
+# Ask for the user's name
+user_name = input("Please enter your name: ")
+
+# Greet the user
+print("Hello, " + user_name + "! Nice to meet you.")
+
