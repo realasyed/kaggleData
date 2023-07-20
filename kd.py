ed070fb833a4d08fa93b0949adc3f768ea079dd5
realasyed <armaansyed120@gmail.com>
2023-07-20 13:35:15 -0700

    testing stuff

--- kd.py
+++ kd.py
@@ -1,3 +1,11 @@
 import streamlit as st 
+import pandas as pd
 
-st.write('Hello, world!')
+#gathering data
+film_data_path = '/home/armaansyed/Programs/Python/kaggleData/filmStats/movie_statistic_dataset.csv'
+data_frame = pd.read_csv(film_data_path) #reads data into data frame. (2d data thing idk. kinda like a cartesian plane)
+
+st.write("test")
+st.dataframe(data_frame)
+
+st.bar_chart(data_frame['test2'])
