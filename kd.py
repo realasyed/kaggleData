902d3e6c8c406a5735955677b012c1357f415794
realasyed <armaansyed120@gmail.com>
2023-07-20 13:40:53 -0500

    bruh

--- kd.py
+++ kd.py
@@ -1,6 +1,4 @@
 import streamlit as st 
 import pandas as pd
 
-#gathering data
-film_data_path = '/home/armaansyed/Programs/Python/kaggleData/filmStats/movie_statistic_dataset.csv'
-data_frame = pd.read_csv(film_data_path) #reads data into data frame. (2d data thing idk. kinda like a cartesian plane)
+st.write('this is a test')
