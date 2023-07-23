7f44850e8e8009edb04c505e4924d0fa72aa585d
realasyed <armaansyed120@gmail.com>
2023-07-23 15:30:17 -0700

    asfdasdf

--- kd.py
+++ kd.py
@@ -1,8 +1,6 @@
 #Comments are for me! If you think they are bad idc!!!! soicanpush
 import torch.cuda as torch
 from torch.cuda import nn
-from torch.utils.data
-from torchvision
 import streamlit as st 
 import pandas as pd #pandas is how we get data into a data frame 
 
